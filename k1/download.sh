f89a74668974c118f0635fabb0f7ac39022b7d1d
Jason Pell <jason@pellcorp.com>
2024-02-17 11:20:34 +1100

    copied a few files over from https://github.com/Guilouz/Creality-K1-and-K1-Max to make minimal installer

---
+++ k1/download.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget "https://raw.githubusercontent.com/Guilouz/Creality-K1-and-K1-Max/main/Scripts/files/moonraker/moonraker.tar.gz" -O install/moonraker.tar.gz
+#wget "https://raw.githubusercontent.com/Guilouz/Creality-K1-and-K1-Max/main/Scripts/files/moonraker/moonraker.conf" -O install/moonraker.conf
+wget "https://github.com/fluidd-core/fluidd/releases/latest/download/fluidd.zip" -O install/fluidd.zip
+
---
+++ k1/install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+nginx_folder="/usr/data/nginx/"
+fluidd_folder="/usr/data/fluidd"
+
+fix_gcode_macros() {
+  sed -i 's/^variable_autotune_shapers:/#&/' /usr/data/printer_data/config/gcode_macro.cfg
+}
+
+install_moonraker() {
+  tar -zxvf install/moonraker.tar.gz -C /usr/data
+  cp /usr/data/nginx/S50nginx /etc/init.d/
+  cp /usr/data/moonraker/S56moonraker_service /etc/init.d/
+  
+  /usr/data/moonraker/moonraker-env/bin/python3 -m pip install --no-dependencies apprise==1.3.0
+  /etc/init.d/S50nginx start
+  sleep 1
+  /etc/init.d/S56moonraker_service start
+  sleep 1
+}
+
+install_fluid() {
+  mkdir -p /usr/data/fluidd
+  unzip -d /usr/data/fluidd install/fluidd.zip
+
+  /etc/init.d/S50nginx restart
+  sleep 1
+  /etc/init.d/S56moonraker_service restart
+  sleep 1
+}
+
+install_guppyscreen() {
+  sh -c "$(wget --no-check-certificate -qO - https://raw.githubusercontent.com/ballaswag/guppyscreen/main/installer.sh)"
+}
+
+fix_gcode_macros
+install_moonraker
+install_fluid
+install_guppyscreen
+
