ef97a7f64e3d0fd4e877f697960559b2805cdfc5
Jason Pell <jason@pellcorp.com>
2024-02-17 12:49:04 +1100

    a few more tweaks

--- k1/install.sh
+++ k1/install.sh
@@ -21,17 +21,18 @@ install_moonraker() {
     exit 1
   fi
   tar -zxf /usr/data/moonraker.tar.gz -C /usr/data
-  rm /usr/data/fluidd.zip
   cp /usr/data/nginx/S50nginx /etc/init.d/
   cp /usr/data/moonraker/S56moonraker_service /etc/init.d/
   
   echo "Updating apprise in moonraker..."
-  /usr/data/moonraker/moonraker-env/bin/python3 -m pip install --no-dependencies apprise==1.3.0
+  /usr/data/moonraker/moonraker-env/bin/python3 -m pip install --no-cache-dir --no-dependencies apprise==1.3.0
+  sync
+  
+  echo "Starting nginx and moonraker..."
   /etc/init.d/S50nginx start
   sleep 1
   /etc/init.d/S56moonraker_service start
   sleep 1
-  sync
 }
 
 install_fluid() {
@@ -44,12 +45,12 @@ install_fluid() {
   mkdir -p /usr/data/fluidd
   unzip -d /usr/data/fluidd /usr/data/fluidd.zip
   rm /usr/data/fluidd.zip
+  sync
   
   /etc/init.d/S50nginx restart
   sleep 1
   /etc/init.d/S56moonraker_service restart
   sleep 1
-  sync
 }
 
 install_guppyscreen() {
