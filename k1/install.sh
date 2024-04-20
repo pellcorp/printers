f08197289a9d74a1d59f1bd13f7cc8e1b4382a3a
Jason Pell <jason@pellcorp.com>
2024-04-20 13:29:08 +1000

    lots of other changes to k1 install

--- k1/install.sh
+++ k1/install.sh
@@ -6,25 +6,27 @@
 # wget "https://github.com/Guilouz/Creality-Helper-Script/blob/main/files/services/S56moonraker_service" -O install/S56moonraker_service
 # wget "https://raw.githubusercontent.com/K1-Klipper/installer_script_k1_and_max/main/sensorless.cfg" -O install/sensorless.cfg
 # wget "https://raw.githubusercontent.com/fluidd-core/fluidd-config/master/client.cfg" -O install/fluidd.cfg
+# wget "https://raw.githubusercontent.com/K1-Klipper/installer_script_k1_and_max/main/S55klipper_service" -O install/S55klipper_service
 
 if [ ! -f /usr/data/printer_data/config/printer.cfg ]; then
   echo "Printer data not setup"
   exit 1
 fi
 
-fix_gcode_macros() {
-  echo "Fixing some gcode config ..."
-  sed -i 's/^variable_autotune_shapers:/#&/' /usr/data/printer_data/config/gcode_macro.cfg
-}
-
 install_k1_klipper() {
   git --depth=1 clone https://github.com/K1-Klipper/klipper.git /usr/data/klipper
-  mv /usr/share/klipper /usr/data/old.klipper
+  rm -rf /usr/data/old.klipper
   ln -s /usr/data/klipper /usr/share/klipper
-  cp /usr/data/printer_data/config/printer.cfg /usr/data/printer_data/config/printer.bak
-	mv /usr/data/printer_data/config/gcode_macro.cfg /usr/data/printer_data/config/gcode_macro.bak
-	mv /usr/data/printer_data/config/sensorless.cfg /usr/data/printer_data/config/sensorless.bak
+  # this is for bltouch
+  sed -i 's/TRSYNC_TIMEOUT = 0.025/TRSYNC_TIMEOUT = 0.050/g' /usr/share/klipper/klippy/mcu.py
+  /usr/share/klippy-env/bin/python3 -m compileall /usr/data/klipper/klippy
   
+  cp install/printer.cfg /usr/data/printer_data/config/
+  cp install/sensorless.cfg /usr/data/printer_data/config/
+  cp install/fluidd.cfg /usr/data/printer_data/config/
+  cp install/gcode_macros.cfg /usr/data/printer_data/config/
+  cp install/S55klipper_service /etc/init.d/
+  /etc/init.d/S55klipper_service restart
 }
 
 install_moonraker() {
@@ -39,7 +41,7 @@ install_moonraker() {
   cp install/S56moonraker_service /etc/init.d/
   cp install/notifier.conf /usr/data/printer_data/config/
   cp install/moonraker.conf /usr/data/printer_data/config/
-    cp install/moonraker.secrets /usr/data/printer_data/
+  cp install/moonraker.secrets /usr/data/printer_data/
   
   echo "Updating apprise in moonraker..."
   /usr/data/moonraker/moonraker-env/bin/python3 -m pip install --no-cache-dir --no-dependencies apprise==1.3.0 || exit $?
@@ -51,7 +53,7 @@ install_moonraker() {
   
   echo "Waiting for moonraker to start ..."
   while true; do
-    MRK_KPY_OK=`curl localhost:7125/server/info | jq .result.klippy_connected`
+    MRK_KPY_OK=`curl localhost:7125/server/info 2> /dev/null | jq .result.klippy_connected`
     if [ "$MRK_KPY_OK" = "true" ]; then
       break;
     fi
@@ -76,11 +78,18 @@ install_fluid() {
 
 install_guppyscreen() {
   echo "Installing guppyscreen..."
-  sh -c "$(wget --no-check-certificate -qO - https://raw.githubusercontent.com/ballaswag/guppyscreen/master/installer.sh)"
+  sh -c "$(wget --no-check-certificate -qO - https://raw.githubusercontent.com/pellcorp/guppyscreen/jp_installer_moonraker_not_started/installer.sh)"
 }
 
-fix_gcode_macros
+function remove_more_services() {
+  BACKUP_DIR=/usr/data/guppyify-backup
+  mv /etc/init.d/S70cx_ai_middleware $BACKUP_DIR
+  mv /etc/init.d/S97webrtc $BACKUP_DIR
+  mv /etc/init.d/S99mdns $BACKUP_DIR
+}
+
 install_moonraker
 install_fluid
 install_guppyscreen
 install_k1_klipper
+remove_more_services
