0b3d6bdfea68635e7a67ea177bd8e50333b4d3f1
Jason Pell <jason@pellcorp.com>
2024-04-22 10:49:13 +1000

    various fixes to get guppyscreen installed

--- k1/install.sh
+++ k1/install.sh
@@ -23,59 +23,27 @@ if [ "$SCRIPTPATH" != "/usr/data/printers/k1" ]; then
   echo "This git repo must be cloned to /usr/data/printers"
 fi
 
-restart_nginx() {
-  /etc/init.d/S50nginx_service stop
-  sleep 10
-  /etc/init.d/S50nginx_service start
-}
-
-restart_klipper() {
-  /etc/init.d/S55klipper_service stop
-  sleep 10
-  /etc/init.d/S55klipper_service start
-}
-
-start_moonraker() {
-  echo "Starting moonraker..."
-  /etc/init.d/S56moonraker_service start
-
-  echo -n "Waiting for moonraker to start ..."
-  while true; do
-    MRK_KPY_OK=`curl localhost:7125/server/info 2> /dev/null | jq .result.klippy_connected`
-    if [ "$MRK_KPY_OK" = "true" ]; then
-      break;
-    fi
-    echo -n "."
-    sleep 1
-  done
-  echo
-
-  # now wait for klipper 
-  while true; do
-    KLIPPER_PATH=`curl localhost:7125/printer/info 2> /dev/null | jq -r .result.klipper_path`
-    if [ -z "$KLIPPER_PATH" -o x"$KLIPPER_PATH" == x"null" ]; then
-      echo -n "."
-      sleep 1
-    else
-      break;
-    fi
- done
- echo
-}
-
 install_k1_klipper() {
+  echo "Installing k1 klipper..."
   git clone https://github.com/pellcorp/k1-klipper.git /usr/data/klipper || exit $?
   rm -rf /usr/share/klipper
   ln -s /usr/data/klipper /usr/share/
+  
+  # make sure we started more limited klipper service
+  cp install/S55klipper_service /etc/init.d/
 
   /usr/share/klippy-env/bin/python3 -m compileall /usr/data/klipper/klippy
 
-  rm /usr/data/printer_data/config/printer_params.cfg
+  # updated creality free config
   cp install/printer.cfg /usr/data/printer_data/config/
   cp install/sensorless.cfg /usr/data/printer_data/config/
   cp install/fluidd.cfg /usr/data/printer_data/config/
   cp install/gcode_macro.cfg /usr/data/printer_data/config/
-  cp install/S55klipper_service /etc/init.d/
+  
+  # remove some unwanted additional config
+  rm /usr/data/printer_data/config/printer_params.cfg
+  rm /usr/data/printer_data/config/factory_printer.cfg
+
   sync
 }
 
@@ -89,7 +57,7 @@ install_moonraker() {
   cp install/moonraker.conf /usr/data/printer_data/config/
   cp install/moonraker.secrets /usr/data/printer_data/
 
-  echo "Updating moonraker to latest from git..."  
+  echo "Updating moonraker to latest..."  
   cd /usr/data/moonraker/moonraker
   git stash
   git checkout master
@@ -99,7 +67,7 @@ install_moonraker() {
   echo "Updating apprise in moonraker..."
   /usr/data/moonraker/moonraker-env/bin/python3 -m pip install --no-cache-dir --no-dependencies apprise==1.3.0 || exit $?
   sync
-  
+
   start_moonraker
 }
 
@@ -116,16 +84,46 @@ install_fluid() {
 install_guppyscreen() {
   cd $PWD
   echo "Installing guppyscreen..."
-  sh -c "$(wget --no-check-certificate -qO - https://raw.githubusercontent.com/pellcorp/guppyscreen/jp_installer_moonraker_not_started/installer.sh)"
-  sync
-}
 
-function remove_more_services() {
-  cd $PWD
   BACKUP_DIR=/usr/data/guppyify-backup
+  K1_GUPPY_DIR=/usr/data/guppyscreen
+  FT2FONT_PATH=/usr/lib/python3.8/site-packages/matplotlib/ft2font.cpython-38-mipsel-linux-gnu.so
+  KLIPPER_PATH=/usr/share/klipper
+  KLIPPY_EXTRA_DIR=$KLIPPER_PATH/klippy/extras
+  GCODE_SHELL_CMD=$KLIPPY_EXTRA_DIR/gcode_shell_command.py
+  SHAPER_CONFIG=$KLIPPY_EXTRA_DIR/calibrate_shaper_config.py
+  K1_CONFIG_DIR=/usr/data/printer_data/config
+
+  tar xf install/guppyscreen.tar.gz -C /usr/data/
+
+  # lets make sure that ssh gets started upon reboot
+  mkdir -p $BACKUP_DIR
+  cp /etc/init.d/S50dropbear $BACKUP_DIR
+  cp $K1_GUPPY_DIR/k1_mods/S50dropbear /etc/init.d/S50dropbear
+
+  mv /etc/init.d/S12boot_display $BACKUP_DIR
+  mv /etc/init.d/S99start_app $BACKUP_DIR
   mv /etc/init.d/S70cx_ai_middleware $BACKUP_DIR
+  # these are extras that guppy install does not normally remove
   mv /etc/init.d/S97webrtc $BACKUP_DIR
   mv /etc/init.d/S99mdns $BACKUP_DIR
+  mv /usr/lib/python3.8/site-packages/matplotlib/ft2font.cpython-38-mipsel-linux-gnu.so $BACKUP_DIR
+  
+  cp $K1_GUPPY_DIR/k1_mods/gcode_shell_command.py $GCODE_SHELL_CMD
+  mkdir -p $K1_CONFIG_DIR/GuppyScreen/scripts
+  cp $K1_GUPPY_DIR/scripts/*.cfg $K1_CONFIG_DIR/GuppyScreen
+  cp $K1_GUPPY_DIR/scripts/*.py $K1_CONFIG_DIR/GuppyScreen/scripts
+
+  cp $K1_GUPPY_DIR/k1_mods/S99guppyscreen /etc/init.d/S99guppyscreen
+
+  cp $K1_GUPPY_DIR/k1_mods/calibrate_shaper_config.py $SHAPER_CONFIG
+  ln -sf $K1_GUPPY_DIR/k1_mods/guppy_module_loader.py $KLIPPY_EXTRA_DIR/guppy_module_loader.py
+  ln -sf $K1_GUPPY_DIR/k1_mods/guppy_config_helper.py $KLIPPY_EXTRA_DIR/guppy_config_helper.py
+  ln -sf $K1_GUPPY_DIR/k1_mods/tmcstatus.py $KLIPPY_EXTRA_DIR/tmcstatus.py
+  cp $K1_GUPPY_DIR/k1_mods/ft2font.cpython-38-mipsel-linux-gnu.so $FT2FONT_PATH
+  ln -sf $K1_GUPPY_DIR/k1_mods/respawn/libeinfo.so.1 /lib/libeinfo.so.1
+  ln -sf $K1_GUPPY_DIR/k1_mods/respawn/librc.so.1 /lib/librc.so.1
+
   sync
 }
 
@@ -133,6 +131,5 @@ install_moonraker
 install_fluid
 install_k1_klipper
 install_guppyscreen
-remove_more_services
 
 echo "You must powercycle the printer so that the nozzle firmware is updated"
